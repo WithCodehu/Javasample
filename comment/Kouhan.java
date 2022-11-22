9d5e4c16a046a1d78f89a60d54b415a62584579c
s.h.u <monqka.apex@gmail.com>
2022-11-22 15:28:56 +0900

    java firstcommit

---
+++ comment/Kouhan.java
@@ -0,0 +1,15 @@
+package comment;
+
+import comment.Zenhan;
+
+public class Kouhan {
+  public static void callDeae() {
+    System.out.println("ええい、小癪な。　曲者だ！であえい！");
+  }
+
+  public static void showMondokoro() throws Exception {
+    System.out.println("飛車さん、角さん、もういいでしょう");
+    System.out.println("この紋所が目に入らぬか！");
+    Zenhan.doToguma();
+  }
+}
---
+++ main/Ct.java
@@ -0,0 +1,12 @@
+package main;
+
+import comment.Zenhan;
+
+public class Ct {
+  public static void main(String[] args) throws Exception {
+    Zenhan.doWarusa();
+    Zenhan.doToguma();
+    comment.Kouhan.callDeae();
+    comment.Kouhan.showMondokoro();
+  }
+}
