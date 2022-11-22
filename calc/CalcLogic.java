ef4030084da2b8f23023a63f66aeb097e24459b0
s.h.u <monqka.apex@gmail.com>
2022-11-22 16:03:28 +0900

    javasample

---
+++ calc/CalcLogic.java
@@ -0,0 +1,16 @@
+package calc;
+
+public class CalcLogic {
+
+    public static int tasu(int a, int b) {
+      int sum = (a + b);
+      return sum;
+    }
+
+    public static int hiku(int a, int b) {
+      int ans = (a - b);
+      return ans;
+    }
+
+  
+}
---
+++ main/CalcMain.java
@@ -0,0 +1,14 @@
+package main;
+import calc.CalcLogic;
+
+public class CalcMain {
+  public static void main(String[] args) {
+
+    int a = 10;
+    int b = 2;
+
+    System.out.println(CalcLogic.tasu(a, b));
+    System.out.println(CalcLogic.hiku(a, b));
+
+  }
+}
